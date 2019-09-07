9ee35939c8f4cedf61a7af173190486a02b4770f
Mustafa Muse <umpp101@icloud.com>
2019-09-07 01:45:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,21 @@
 def new_hash
-  # return an empty hash
+  puts new_hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  puts my_hash[:id = "Mustafa"
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  puts pioneer[:name]='Grace Hopper'
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator[:id] = 1
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator[key] = value
 end
 
 def read_from_hash(hash, key)
